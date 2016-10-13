e65811b137f2d7c048837944ba1631aa649d1a5e
u982025 <u982025@yahoo.com>
2016-10-12 21:17:32 -0700

    test

--- test2.py
+++ test2.py
@@ -6,9 +6,9 @@ def passwd():
         yield f.readlines()
 
 def test_has_lines(passwd):
-    print('testing test has lines")
+    print('testing test has lines')
     assert len(passwd) >= 301
 
 def test_nos_lines(passwd):
-    print('testing test has no lines")
+    print('testing test has no lines')
     assert len(passwd) >= 1
