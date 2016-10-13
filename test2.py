1ebad8bf5fb076cbc15bc648387c0fe9dfce6743
u982025 <u982025@yahoo.com>
2016-10-12 21:09:58 -0700

    "v5"

--- test2.py
+++ test2.py
@@ -6,4 +6,9 @@ def passwd():
         yield f.readlines()
 
 def test_has_lines(passwd):
+    print('testing test has lines")
     assert len(passwd) >= 301
+
+def test_nos_lines(passwd):
+    print('testing test has no lines")
+    assert len(passwd) >= 1
