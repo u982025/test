0c517740f3a11fc908069aa1a1d109d0375bf4c2
yabebe <administrator@Hackintosh.attlocal.net>
2016-10-22 19:03:26 -0700

    added elapsed time

---
+++ elapsed_time.py
@@ -0,0 +1,11 @@
+import time
+def make_timer():
+    start_time = None
+    def elapsed():
+        nonlocal start_time
+        now = time.time()
+        if start_time is None:
+            start_time = now
+            return None
+        return (now - start_time)
+    return elapsed
