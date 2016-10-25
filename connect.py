373da6f6b92589be8c12367dbc613e5175481cb4
yabebe <administrator@Hackintosh.local>
2016-10-25 11:34:29 -0700

    added connect and exec_cmd

--- connect.py
+++ connect.py
@@ -1,8 +1,12 @@
 from pexpect import pxssh
 def connect(host, user, password, test_cmd='uptime'):
-    connId = pxssh.pxssh(options={"StrictHostKeyChecking": "no",  "UserKnownHostsFile": "/dev/null"})
+    """
+    Connects to a linux host and returns spawn id
+    """
+    uptimePattern = re.compile('\d{1,2}:\d{1,2}:\d{1,2}\sup\s+\d{1,2}\smin')
+    spawnId = pxssh.pxssh(options={"StrictHostKeyChecking": "no",  "UserKnownHostsFile": "/dev/null"})
     try: 
-        spawnId.login(host, user, password): 
+        spawnId.login(host, user, password)
     except ExceptionPxssh:
         return 0
     finally:
@@ -10,4 +14,20 @@ def connect(host, user, password, test_cmd='uptime'):
         if spawnId.prompt():
             retVal = spawnId.before
             for line in retVal.split('\r\n'):
-                if 
+                if uptimePattern.search(line):
+                    isHostUp = True
+                    return spawnId
+    return 0
+ 
+
+def exec_cmd(spawn_id, cmd):
+    """
+    Executes command on a Linux host and return the output
+    """
+    retVal = ''
+    if not spawn_id.isalive():
+        return None
+    spawn_id.sendline(cmd)
+    if not spawn_id.prompt():
+        return None
+    return spawn_id.before 
